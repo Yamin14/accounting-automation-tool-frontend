a1ec6979f1ca0603374396f87f01b8e433e1314e
Yamin14 <syalikazmi@gmail.com>
2025-12-09 03:38:22 -0800

    Prod URL Fixed

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:3000/api',
-  baseURL: 'https://accounting-automation-tool-backend.vercel.app',
+  baseURL: 'https://accounting-automation-tool-backend.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
